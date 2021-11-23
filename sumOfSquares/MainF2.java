ecdaee0bd6e07e2681ab65e384f9f2843311c8f2
IlhamAyZa <IZADA20@KU.EDU.TR>
2021-11-23 20:47:53 +0300

    Update MainF2.java

--- sumOfSquares/MainF2.java
+++ sumOfSquares/MainF2.java
@@ -4,12 +4,22 @@ public class MainF2 implements FunctionMV {
 
     public void main(String[] args) {
 
-        System.out.println();
-        Point point = new Point(3);
-        double[] aPoint = {1, 2, 1};
-        point.setComponents(aPoint);
+        public void main(String[] args) {
 
-        System.out.println(fx(point.getComponents()));
+        System.out.println();
+        double[] aPoint = {1, 2, 1};
+
+        System.out.println(fx(aPoint));
+
+    }
+
+    public double fx(double[] x) {
+        double diff = Math.pow(x[0], 2);
+        double a = Math.pow(1 - x[0], 2);
+        double b = Math.pow(x[1] - diff, 2);
+
+        return a + (100 * b);
+    }
 
     }
 
