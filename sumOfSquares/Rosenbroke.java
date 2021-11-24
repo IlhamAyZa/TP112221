ec19fea6a19805d146c6f2fb1e3a38f11397c399
IlhamAyZa <IZADA20@KU.EDU.TR>
2021-11-24 10:52:46 +0300

    Add files via upload

---
+++ sumOfSquares/Rosenbroke.java
@@ -0,0 +1,13 @@
+package sumOfSquares;
+
+public class Rosenbroke implements FunctionMV {
+
+    @Override
+    public double fx(double[] x) {
+        double diff = Math.pow(x[0], 2);
+        double a = Math.pow(1 - x[0], 2);
+        double b = Math.pow(x[1] - diff, 2);
+
+        return a + (100 * b);
+    }
+}
